9a46768345e270ca5e9a102362743ab40dbbe1a3
hejny <me@pavolhejny.com>
2018-01-20 06:12:05 +0100

    + reaction

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,58 @@
 let reaction = ""
 
+
+//const elementsCache: {element: HTMLElement; reaction: number}[] = [];
+
+
+
+
 function superHey() {
 
-    const list = document.body.querySelectorAll('div')
+    const list = document.body.querySelectorAll('div[data-reaction]');
     for (var i = list.length-1; i >= 0; i--) {
         const buttonElement: any = list[i];
-        if (/^.*Super.*$/g.test(buttonElement.innerText)) {
-            buttonElement.removeEventListener('click', clickListener, false)
-            buttonElement.addEventListener('click', clickListener, false)
-        }
+
+        /*elementsCache.push({
+            element: buttonElement,
+            reaction: buttonElement.getAttribute('data-reaction')
+
+        });*/
+
+
+        buttonElement.style.border = '1px solid red';
+        //buttonElement.setAttribute('data-my',buttonElement.getAttribute('data-reaction'));
+
+
+        const clickListener = createClickListener(parseInt(buttonElement.getAttribute('data-reaction')));
+
+
+        buttonElement.removeEventListener('click', clickListener, false)
+        buttonElement.addEventListener('click', clickListener, false)
+
     }
 }
 
-function clickListener(event: MouseEvent) {
-    let urlik = findUserId(event.target as HTMLElement)
-    alert(urlik)
+
+
+
+const clickListenersCache: {
+    [key: number]: (event: MouseEvent) => never
+} = {};
+function createClickListener(reaction:number){
+    return clickListenersCache[reaction] = clickListenersCache[reaction]||clickListener.bind(null, reaction);
+}
+
+
+function clickListener(reaction:number , event: MouseEvent) {
+
+    const element = event.target as HTMLElement;
+
+    //const reaction = elementsCache.find((item)=>item.element===element);
+
+    console.log(reaction);
+
+    let url = findUserId(element);
+    alert(`Reaction ${reaction} on page "${url}".`);
 }
 
 setInterval(() => {
@@ -28,7 +66,7 @@ function _findUserIdOnSelfOfChildren(element: HTMLElement):string[] {//todo not 
         const url = element.getAttribute('href');
         if(typeof url === 'string'){
             if (/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g.test(url)) {
-                console.log(url)
+                //console.log(url)
                 if (!/https?:\/\/(.*)?facebook\.com\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g.test(url)) {
                   userIds.push(url);  
                 }
@@ -41,7 +79,7 @@ function _findUserIdOnSelfOfChildren(element: HTMLElement):string[] {//todo not 
             userIds.push(userId);
         }
     }
-    console.log(userIds)
+    //console.log(userIds)
     return userIds
 }
 
@@ -54,7 +92,7 @@ function findUserId(element: HTMLElement):string {
         if(element.parentElement) {
             return findUserId(element.parentElement);
         }else{
-            console.log(element);
+            //console.log(element);
             throw new Error('Cant get any user id.');
         }
     }else
